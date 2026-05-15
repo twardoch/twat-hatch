0282ca622cdb54d49d3de32253b0b71a52e9b5ca
Adam Twardoch <adam@twardoch.com>
2026-05-16 00:53:30 +0200

    v2.7.6

--- src/twat_hatch/__init__.py
+++ src/twat_hatch/__init__.py
@@ -1,8 +1,7 @@
 """Package initialization functionality for twat."""
 
-from importlib import metadata
+from .__version__ import __version__  # noqa: F401
 
 from twat_hatch.hatch import PackageInitializer
 
-__version__ = metadata.version(__name__)
 __all__ = ["PackageInitializer", "__version__"]
